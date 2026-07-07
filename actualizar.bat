76abce30416feed2eb3a00b561278c261fafbfc0
TChesare2608 <Chesaresuport@gmail.com>
2026-07-07 05:17:30 -0600

    Actualizacion de modpack desde desarrollo

--- actualizar.bat
+++ actualizar.bat
@@ -7,11 +7,6 @@ git add .
 git commit -m "Actualizacion de modpack desde desarrollo"
 git push origin main
 
-echo [DESARROLLO] Enviando orden de actualizacion al servidor...
-:: Si el servidor esta en la misma PC por ahora, solo llamamos al otro bat.
-:: En el futuro, aqui pondras: ssh usuario@ip-servidor "C:\Ruta\servidor\actualizar_srv.bat"
-call "C:\Users\Chesa\OneDrive\Desktop\SELVELPOLQUEMON\actualizarsv.bat"
-
 echo ==================================================
 echo ¡Todo sincronizado!
 echo ==================================================
